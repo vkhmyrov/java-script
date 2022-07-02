c562bfd06243c634c452775683c4fc64435be62e
Vadym Khmyrov <v.khmyrov@yahoo.com>
2022-07-02 23:33:59 +0300

    Update js-var-let/js/script.js

--- js-var-let/js/script.js
+++ js-var-let/js/script.js
@@ -3,5 +3,26 @@ alert ('The first message')
 var name = 'Vladilen'
 const LastName = 'Minin'
 
+
 name = 'Vlad'
-console.log(name)
+console.log(LastName)
+
+// It is my first coment here
+
+let age = 30
+age = 40
+console.log(age)
+
+// It is my next comment here
+
+const validatedForm = true
+console.log(validatedForm)
+
+//
+
+age = false
+console.log(age)
+
+//
+
+
