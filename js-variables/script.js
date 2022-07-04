5a6bcac2aa3d21f536ad298afe07fa35229bb9c9
Vadym Khmyrov <v.khmyrov@yahoo.com>
2022-07-04 20:23:02 +0300

    Add script.js into folder js-variables

---
+++ js-variables/script.js
@@ -0,0 +1,96 @@
+// Displaing pop-up on screen
+
+alert ('The first message')
+
+
+// var & const
+
+var name = 'Vladilen'
+const surname = 'Minin'
+console.log (name, surname)
+
+
+// Name of camelCase
+
+let firstName = 'Ivan'
+const lastName = 'Gromov'
+let age = 30
+console.log (firstName,lastName,age)
+
+
+// Name variables
+
+const FirstName = 'Alex'
+const LastName = 'Pupock'
+console.log (FirstName, LastName)
+
+const username = 'Vladilen'
+const userlastname = 'Minin'
+console.log (username, userlastname)
+
+const _private = 'private'
+console.log (_private)
+
+const _ = " _ "
+console.log (_)
+
+const $ = "$"
+console.log ($)
+
+const field5 = 'field #5'
+console.log (field5)
+
+// const if = 'mkef' // error of if. it is reservetion veriable
+// const 5num = 'number #5'  // error of first number 5. The first simbol is only text
+
+
+// Change variable
+
+name = 'Vlad'
+age = 40
+console.log(name, age)
+
+
+
+// Uppercase constans 
+
+const COLOR_RED = '#F00'
+const COLOR_GREEN = '#0f0'
+const COLOR_BLUE = '#00f'
+const COLOR_ORANGE = '#ff7f00'
+
+let color = COLOR_GREEN
+console.log (color)
+
+
+// Task practice #1
+
+let name2 = 'Джон'
+let admin
+
+admin = name2 
+alert (admin)
+
+let admin1, name3
+name3 = 'Bro'
+admin1 = name3
+
+alert (admin1)
+
+
+// Task practice #2
+
+let nameEarth = 'Земля'
+let userName3 = 'Fafa'
+
+
+
+
+
+
+
+
+
+
+
+
