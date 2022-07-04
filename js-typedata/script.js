7a1b6f8d24a5703bc4f579de72355fbc3d70f7a9
Vadym Khmyrov <v.khmyrov@yahoo.com>
2022-07-04 21:41:17 +0300

    Update js-typedata/script.js

--- js-typedata/script.js
+++ js-typedata/script.js
@@ -1,5 +1,32 @@
 // Value variables
 
+let message = "hello"
+message = 123;
+alert (message)
+
+// Numbet
+
+let n = 12345
+n = 555
+console.log (n)
+
+
+// Infinity
+
+alert (1/0)
+
+alert (Infinity)
+
+
+// NaN
+
+alert ("String"/2) 
+alert (NaN+1)
+alert (3*NaN)
+
+
+//
+
 const validatedForm = true //boolean
 console.log(validatedForm)
 
