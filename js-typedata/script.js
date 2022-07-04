ab9dbca9ff059b931dac12a33bb63883be21212d
Vadym Khmyrov <v.khmyrov@yahoo.com>
2022-07-04 20:30:55 +0300

    Add js-typedata/script.js

---
+++ js-typedata/script.js
@@ -0,0 +1,13 @@
+// Value variables
+
+const validatedForm = true //boolean
+console.log(validatedForm)
+
+age = false //boolean
+console.log(age)
+
+age = 50 //number
+console.log (age)
+
+firstName = 'Ivanko' //string 
+console.log (firstName)
