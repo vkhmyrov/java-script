4c32436ecca077229a628d3a9d2e8e4497bfeb91
Vadym Khmyrov <v.khmyrov@yahoo.com>
2022-07-06 20:39:07 +0300

    Add js-alert,prompt,confirm/

---
+++ js-alert,prompt,confirm/script.js
@@ -0,0 +1,28 @@
+// alert
+
+const firstName = 'Vladilen'
+const age = 26
+console.log ('User Name:' + firstName + ', and his age ' + age)
+
+alert ('User Name:' + firstName + ', and his age:' + age)
+
+
+// prompt
+
+const lastName2 = prompt ('Enter your last name')
+alert (firstName + ' ' + lastName2)
+
+let age1 = prompt ("How older you?", 50 )
+alert (`Do you have ${age1}!`)
+
+
+// confirm
+
+let newsLetter = confirm ('Do you have receive notification?')
+alert (newsLetter)
+
+
+// Task practice #1
+
+let name = prompt ('What are you name?','')
+alert (name)
