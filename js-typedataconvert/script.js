b5657aa4a56658c53d2876681ebc1ba96b0ce812
Vadym Khmyrov <v.khmyrov@yahoo.com>
2022-07-07 12:07:11 +0300

    Add number conversion into js-typedataconvert/script.js

--- js-typedataconvert/script.js
+++ js-typedataconvert/script.js
@@ -5,3 +5,16 @@ alert (typeof value) // boolean
 
 value = String (value)
 alert (typeof value) // string
+
+
+// Number conversion
+
+alert ("6"/"2");
+
+let str = '123';
+alert (typeof str);
+let num = Number (str);
+alert (typeof num);
+
+let hello = Number ("Hello");
+alert (hello);
