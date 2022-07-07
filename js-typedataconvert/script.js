751257d1356c8c73ce0f1ec404d7354286c65163
Vadym Khmyrov <v.khmyrov@yahoo.com>
2022-07-07 12:48:24 +0300

    Add logical conversion into js-typedataconvert/script.js

--- js-typedataconvert/script.js
+++ js-typedataconvert/script.js
@@ -18,3 +18,50 @@ alert (typeof num);
 
 let hello = Number ("Hello");
 alert (hello);
+
+
+// Logical conversion
+
+let userAge = 58;
+console.log (userAge);
+console.log (typeof userAge);
+
+userAge = Boolean (userAge);
+console.log (userAge);
+console.log (typeof userAge);
+
+
+let userAge1 = 0;
+console.log (userAge1);
+console.log (typeof userAge1);
+
+userAge1 = Boolean (userAge1);
+console.log (userAge1);
+console.log (typeof userAge1);
+
+
+let userAge2 = "0";
+console.log (userAge2);
+console.log (typeof userAge2);
+
+userAge2 = Boolean (userAge2);
+console.log (userAge2);
+console.log (typeof userAge2);
+
+
+let userAge3 = "_";
+console.log (userAge3);
+console.log (typeof userAge3);
+
+userAge3 = Boolean (userAge3);
+console.log (userAge3);
+console.log (typeof userAge3);
+
+
+let userAge4 = "";
+console.log (userAge4);
+console.log (typeof userAge4);
+
+userAge4 = Boolean (userAge4);
+console.log (userAge4);
+console.log (typeof userAge4);
